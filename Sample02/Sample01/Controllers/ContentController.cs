37e3adaf0854dda739a875b66e728125d562c78b
wutr <810854645@qq.com>
2018-12-11 13:26:33 +0800

    新增第三章文章的事例代码

---
+++ Sample02/Sample01/Controllers/ContentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sample01.Models;
+
+namespace Sample01.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly Content contents;
+        public ContentController(IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+        /// <summary>
+        /// 首页显示
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+
+            return View(new ContentViewModel { Contents = new List<Content> { contents } });
+        }
+    }
+}
